1a491a10674c0dbc75f20b27129a73db81d0a4d0
Geeknerd1337 <geeknerd1337@gmail.com>
2022-04-10 15:26:02 -0500

    Further Schema Updates, Drop Schema Script Added

--- DB-Schema.sql
+++ DB-Schema.sql
@@ -1,14 +1,4 @@
 use cse;
-create table payroll(
-payment_ID int primary key,
-emp_ID int,
-total_wage int,
-hours_worked int,
-overtime int,
-bonus_pay int,
-payment_method varchar(255),
-foreign key (emp_ID) references employee(emp_ID)
-);
 
 create table department(
 department_ID int primary key,
@@ -23,6 +13,29 @@ alcohol_sales boolean,
 money_handling boolean
 );
 
+create table employee(
+emp_ID int primary key,
+first_name varchar(255) NOT NULL,
+middle_initial varchar(1) NULL,
+last_name varchar(255) NOT NULL,
+department_ID int NOT NULL,
+license_ID int NOT NULL,
+foreign key (department_ID) references department(department_ID),
+foreign key (license_ID) references licenses(license_ID)
+);
+
+create table payroll(
+payment_ID int primary key,
+emp_ID int,
+total_wage int,
+hours_worked int,
+overtime int,
+bonus_pay int,
+payment_method varchar(255),
+foreign key (emp_ID) references employee(emp_ID)
+);
+
+
 create table employee_schedule(
 schedule_ID int primary key,
 emp_ID int,
@@ -33,51 +46,79 @@ is_pto bool NOT NULL,
 foreign key (emp_ID) references employee(emp_ID)
 );
 
-create table employee(
-emp_ID int primary key,
-first_name varchar(255) NOT NULL,
-middle_initial varchar(1) NULL,
-last_name varchar(255) NOT NULL,
-department_ID int NOT NULL,
-liscense_ID int NOT NULL,
-foreign key (department_ID) references department(department_ID),
-foreign key (liscense_ID) references licenses(liscense_ID)
-)
+create table region(
+region_ID int primary key,
+annual_profit float NOT NULL,
+state varchar(255) NOT NULL
+);
 
 create table store(
 store_ID int primary key,
 region_ID int NOT NULL,
-
-)
-
-create table address(
-
-)
-
-
-create table aisle(
+address_ID int NOT NULL,
+phone_ID int NOT NULL,
+num_employees int NOT NULL,
+num_gas_pumps int NOT NULL,
+phone_number varchar(255) NOT NULL,
+store_address varchar(255) NOT NULL,
+foreign key (region_ID) references region(region_ID)
 );
 
-create table aisle_product_assoc(
-
+create table management(
+manager_ID int primary key,
+first_name varchar(255) NOT NULL,
+middle_initial varchar(1) NULL,
+last_name varchar(255) NOT NULL,
+region_ID int NOT NULL,
+foreign key (region_ID) references region(region_ID)
 );
 
-create table product(
-
+create table card_reader(
+card_reader_ID int,
+serial_number int,
+total_transactions int NOT NULL,
+dollars_collected_daily float NOT NULL,
+PRIMARY KEY(card_reader_ID, serial_number)
 );
 
-create table product_invoice_assoc(
-
+create table pump(
+pump_ID int primary key,
+local_ip varchar(255) NOT NULL,
+date_installed date NOT NULL,
+store_ID int NOT NULL,
+gallons_sold double NOT NULL,
+dispense_rate double NOT NULL,
+last_maintenance date NOT NULL,
+card_reader_ID int NOT NULL,
+foreign key (card_reader_ID) references card_reader(card_reader_ID),
+foreign key (store_ID) references store(store_ID)
 );
 
-create table purchase_invoice(
-
+create table gas_type(
+gas_type_ID int primary key,
+price double NOT NULL,
+type_name varchar(255) NOT NULL
 );
 
-create table supplier_invoice_assoc(
 
+create table pump_gas_type_assoc(
+pump_ID int not null,
+gas_type_ID int not null,
+PRIMARY KEY(pump_ID, type_ID),
+foreign key (pump_ID) references pump(pump_ID),
+foreign key (gas_type_ID) references gas_type(gas_type_ID)
 );
 
-create table product_supplier(
-
+create table resevoir(
+resevoir_ID int primary key,
+gas_type_ID int not null,
+holding_capacity double not null,
+current_capacity double not null,
+foreign key (gas_type_ID) references gas_type(gas_type_ID)
 );
+
+
+
+
+
+
---
+++ DROP-DB.sql
@@ -0,0 +1,15 @@
+SET FOREIGN_KEY_CHECKS = 0;
+drop table if exists card_reader;
+drop table if exists department;
+drop table if exists employee;
+drop table if exists employee_schedule;
+drop table if exists gas_type;
+drop table if exists licenses;
+drop table if exists management;
+drop table if exists payroll;
+drop table if exists pump;
+drop table if exists pump_gas_type_assoc;
+drop table if exists region;
+drop table if exists resevoir;
+drop table if exists store;
+SET FOREIGN_KEY_CHECKS = 1;
